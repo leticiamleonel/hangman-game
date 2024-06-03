0fea9c41657ceb9e652b82daab954a44edff1dd1
leticiamleonel <leticiamoreira0318@gmail.com>
2024-06-03 09:40:30 -0300

    vFinal

--- hangmanGame/main.cpp
+++ hangmanGame/main.cpp
@@ -11,7 +11,7 @@ void limpaTela(){
 
 string retornaPalavraAleatoria(){
     //vetor com palavras disponíveis
-    string palavras[3] = {"Maça", "Morango", "Manga"};
+    string palavras[3] = {"caju", "morango", "manga"};
     //indice gerado no intervalo 0,1,2
     int indiceAleatorio = rand() % 3;
 
@@ -30,13 +30,21 @@ string retornaPalavraComMascara(string palavra, int tamanhoDaPalavra){
     return palavraComMascara;
 }
 
-void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes ){
+void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes, string letrasArriscadas){
     cout << "A palavra é: " << palavraComMascara << "(Tamanho: " << tamanhoDaPalavra << ")" << endl;
     cout << "Tentativas Restantes: " << tentativasRestantes << endl;
 
+    int cont;
+    //exibe as letras arriscadas
+    cout << "Letras já arriscadas: " << endl;
+    for(cont = 0; cont < letrasArriscadas.size(); cont ++){
+        cout << letrasArriscadas[cont] << ", ";
+    }
+
+
 }
 
-void jogarSozinho(){
+int jogarSozinho(){
     //palavra a ser advinhada
     string palavra = retornaPalavraAleatoria();
     //pega o tamanho da palavra | Ex: Morango = 7 letras
@@ -46,16 +54,58 @@ void jogarSozinho(){
 
     int tentativas = 0, maximoTentativas = 10; //numero de tentativas e erros
     char letra; //letra de entrada do usuário
+    int opcao;
     int cont; //auxiliar para laços de repetição
+    string letrasArriscadas; //acumula as tentativas do jogador
+    bool letraJaDigitada = false;
 
-    while(maximoTentativas - tentativas >0){
+    while(palavra != palavraComMascara && maximoTentativas - tentativas > 0){
         limpaTela();
         //exibe o status atual do jogo
-        exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoTentativas - tentativas);
+        exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoTentativas - tentativas, letrasArriscadas);
         //palpite
-        cout << "Digite uma letra: ";
+        cout << "\nDigite uma letra: ";
         cin >> letra;
-        tentativas++;
+
+        //percorre as letras já arriscadas
+        for(cont = 0; cont < tentativas; cont++){
+            if(letrasArriscadas[cont] == letra){
+                cout << "Essa letra já foi digitada!" << endl;
+                letraJaDigitada = true;
+            }
+        }
+
+        if(letraJaDigitada == false){
+            //incrementa as letras já arriscadas
+            letrasArriscadas += tolower(letra);
+            //percorre a letra real
+            for(cont=0; cont<tamanhoDaPalavra; cont++){
+            //se a letra existir na palavra com máscara
+                if (palavra[cont]== tolower(letra)){
+
+                //faço aquela letra aparecer naquela palavra com máscara
+                    palavraComMascara[cont] = palavra[cont];
+                }
+            }
+            tentativas++;
+        }
+        letraJaDigitada = false;
+
+    }
+    if(palavra == palavraComMascara){
+        limpaTela();
+        cout << "Parabéns, Você venceu. A palavra era: " << palavra << "!"<< endl;
+        cout << "Deseja reiniciar?" << endl;
+        cout << "Digite: Sim = 1 ou Não = 2" << endl;
+        cin >> opcao;
+        return opcao;
+    }else{
+        limpaTela();
+        cout << "Sem tentativas! Game Over!" << endl;
+        cout << "Deseja reiniciar?" << endl;
+        cout << "Digite: Sim = 1 ou Não = 2" << endl;
+        cin >> opcao;
+        return opcao;
     }
 
 
@@ -80,10 +130,20 @@ void menuInicial(){
         switch(opcao){
         case 1:
             cout << "__ Início do jogo __" <<endl;
-            jogarSozinho();
+            if(jogarSozinho() == 1){
+                menuInicial();
+            }
             break;
         case 2:
             cout << "__ Informações do jogo __" <<endl;
+            limpaTela();
+            cout << "Jogo desenvolvido por Letícia Leonel, em 2024.";
+            cout << "\n1 - Voltar";
+            cout << "\n2 - Sair" << endl;
+            cin >> opcao;
+            if(opcao==1){
+                menuInicial();
+            }
             break;
         case 3:
             cout << "__ Fim. Até logo! __" <<endl;
